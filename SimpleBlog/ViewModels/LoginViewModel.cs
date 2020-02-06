6cf501a06e3eb376078e0c8b495be7b5a0acc948
Гелцен Савындай <5842_sgy@keva.int>
2020-02-06 19:57:56 +0700

    Added basic styling

--- SimpleBlog/ViewModels/LoginViewModel.cs
+++ SimpleBlog/ViewModels/LoginViewModel.cs
@@ -1,8 +1,11 @@
-﻿namespace SimpleBlog.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SimpleBlog.ViewModels
 {
     public class LoginViewModel
     {
         public string UserName { get; set; }
+        [DataType(DataType.Password)]
         public string Password { get; set; }
     }
 }
